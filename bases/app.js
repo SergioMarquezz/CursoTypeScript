6ef95c52c82773e484ba8243db1370792d3f4921
Sergio Alberto Marquez Moreno <serdevic1926@gmail.com>
2023-03-12 22:03:09 -0600

    Primer Commit para el cuerso de TypeScript

---
+++ bases/app.js
@@ -0,0 +1,9 @@
+"use strict";
+//let msj = 'Hola Mundo';
+const msj = 'Hola';
+const hero = {
+    name: 'Iroman',
+    age: 27
+};
+hero.age = 50;
+console.log(hero.age + 1);
---
+++ bases/app.ts
@@ -0,0 +1,11 @@
+//let msj = 'Hola Mundo';
+const msj: string = 'Hola';
+
+const hero = {
+    name: 'Iroman',
+    age: 27
+}
+
+hero.age = 50;
+
+console.log(hero.age + 1);
