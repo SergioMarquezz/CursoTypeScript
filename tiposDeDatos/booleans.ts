2d962d6d15375d96b27d1cb29d1969bbb3b9dc79
Sergio Alberto Marquez Moreno <serdevic1926@gmail.com>
2023-03-12 22:39:41 -0600

    Tipo de dato boolean

---
+++ tiposDeDatos/booleans.ts
@@ -0,0 +1,8 @@
+(()=>{
+    let isSuperman:boolean = true;
+    let isBatman:boolean = false;
+
+    isSuperman = (isBatman) ? true : false;
+
+    console.log({isBatman}); //Imprime la variable y el valor
+})()
