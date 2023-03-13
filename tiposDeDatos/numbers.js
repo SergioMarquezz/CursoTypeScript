f9a8e2c9ea9c901d228c154e1c7b51704e999cd3
Sergio Alberto Marquez Moreno <serdevic1926@gmail.com>
2023-03-12 22:52:15 -0600

    Tipo de dato number

---
+++ tiposDeDatos/numbers.js
@@ -0,0 +1,12 @@
+"use strict";
+(() => {
+    let vengadores = 10;
+    console.log(vengadores);
+    const villanos = 20;
+    if (vengadores < villanos) {
+        console.log("Estamos en problemas");
+    }
+    else {
+        console.log("Nos salvamos");
+    }
+})();
---
+++ tiposDeDatos/numbers.ts
@@ -0,0 +1,14 @@
+(()=>{
+
+    let vengadores:number = 10;
+    console.log(vengadores);
+    const villanos:number = 20;
+
+    if(vengadores < villanos){
+        console.log("Estamos en problemas");
+    }
+    else{
+        console.log("Nos salvamos");
+    }
+
+})()
