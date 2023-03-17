1af208ada869200fbbf95c954dbd399b225c8326
Sergio Alberto Marquez Moreno <serdevic1926@gmail.com>
2023-03-16 22:05:33 -0600

    Tipo de dato arreglos en TypeScript

---
+++ tiposDeDatos/arrays.js
@@ -0,0 +1,10 @@
+"use strict";
+(() => {
+    //const numbers: (string | number | boolean)[] = [1,2,3,4,5,6,7,8,9,10,'Sergio'];
+    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    numbers.push(27);
+    //numbers.push(true);
+    console.log(numbers);
+    const heroes = ['Superman', 'Batman', 'Capitan America', 'Thor'];
+    heroes.forEach(h => console.log(h.toUpperCase()));
+})();
