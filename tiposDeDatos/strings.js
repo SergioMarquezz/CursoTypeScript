360d964771c2dfc94fcd0e5ba900c3eb12649acc
Sergio Alberto Marquez Moreno <serdevic1926@gmail.com>
2023-03-16 21:43:03 -0600

    Tipo de dato Strings TypeScript

---
+++ tiposDeDatos/strings.js
@@ -0,0 +1,8 @@
+"use strict";
+(() => {
+    const batman = 'Batman';
+    const superman = "SuperMan";
+    const spiderman = `Super Heroe`;
+    console.log(`Hola ' ${spiderman}`);
+    console.log(batman.toUpperCase());
+})();
---
+++ tiposDeDatos/strings.ts
@@ -0,0 +1,10 @@
+(() =>{
+
+    const batman:string = 'Batman';
+    const superman:string = "SuperMan"
+    const spiderman:string = `Super Heroe`
+
+    console.log(`Hola ' ${spiderman}`)
+    console.log(batman.toUpperCase());
+
+})()
