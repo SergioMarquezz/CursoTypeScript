85c390c806167b0ef20e4823426c0d1d6c6339a2
Sergio Alberto Marquez Moreno <serdevic1926@gmail.com>
2023-03-16 21:53:41 -0600

    Tipo de dato en TypeScript any

---
+++ tiposDeDatos/any.js
@@ -0,0 +1,10 @@
+"use strict";
+(() => {
+    let vengador = 123;
+    let exists;
+    let power;
+    vengador = "Capitan America";
+    console.log(vengador.charAt(0));
+    vengador = 150.565646;
+    console.log(vengador.toFixed(2));
+})();
---
+++ tiposDeDatos/any.ts
@@ -0,0 +1,14 @@
+(()=>{
+
+    let vengador:any = 123;
+    let exists;
+    let power;
+
+    vengador = "Capitan America";
+
+    console.log(vengador.charAt(0));
+
+    vengador = 150.565646
+    console.log(vengador.toFixed(2));
+
+})()
