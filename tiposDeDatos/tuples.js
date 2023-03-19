207b730338d9e2abb4581782c4aab452b097fd69
Sergio Alberto Marquez Moreno <serdevic1926@gmail.com>
2023-03-19 17:10:15 -0600

    Tipo de dato en TypeScript tuplas

---
+++ tiposDeDatos/tuples.js
@@ -0,0 +1,9 @@
+"use strict";
+(() => {
+    /*En las duplas podemos controlar los valores
+    y tipos de datos que tendra la dupla*/
+    const hero = ['Dr. Strange', 100];
+    hero[0] = 'Ironman';
+    hero[1] = 50;
+    console.log(hero);
+})();
---
+++ tiposDeDatos/tuples.ts
@@ -0,0 +1,12 @@
+(()=>{
+
+    /*En las duplas podemos controlar los valores
+    y tipos de datos que tendra la dupla*/ 
+    const hero:[string, number] = ['Dr. Strange',100]
+
+    hero[0] = 'Ironman';
+    hero[1] = 50;
+    
+    console.log(hero);
+
+})()
